52b671f20fe0b8952c5678b0ad899112579a85b0
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-08-10 16:31:25 -0300

    added user registration method

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,6 +1,16 @@
+/* eslint-disable indent */
 'use strict'
 
+const User = use('App/Models/User')
+
 class UserController {
+    async store ({ request }) {
+        const data = request.only(['username', 'email', 'password'])
+
+        const user = await User.create(data)
+
+        return user
+    }
 }
 
 module.exports = UserController
