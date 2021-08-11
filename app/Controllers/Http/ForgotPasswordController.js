8eff3488c0a3d101edd8e421bd25fedf6ed5e5ea
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-08-11 13:05:25 -0300

    created reset password email forwarding method

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-trailing-spaces */
 /* eslint-disable indent */
 'use strict'
 
 const crypto = require('crypto')
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
     async store ({ request, response }) {
@@ -14,6 +16,21 @@ class ForgotPasswordController {
             user.token_created_at = new Date()
 
             await user.save()
+
+            await Mail.send(
+                ['emails.forgot_password'],
+                { 
+                    email, 
+                    token: user.token, 
+                    link: `${request.input('redirect_url')}?token=${user.token}` 
+                },
+                message => {
+                    message
+                    .to(user.email)
+                    .from('emanuboy@emanuboy.com', 'Emanu | Boy')
+                    .subject('Recuperação de senha')
+                }
+            )
         } catch (err) {
             return response.status(err.status).send({ error: { message: 'Algo não deu certo, esse email existe?' } })
         }
